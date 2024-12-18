72ff7710bdc09d43e3395b9703f4ca0b3a7e3a65
Israel0li <israeloliveirarocha1@gmail.com>
2024-12-18 12:35:43 -0300

    .

---
+++ aula3.py
@@ -0,0 +1,21 @@
+"""
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+Strings são textos que estão dentro das aspas
+"""
+print(1234)
+
+# Aspas simples
+print('Israel Oliveira')
+print(1, 'Israel "Oliveira"')
+
+# Aspas duplas
+print("Israel Olivera")
+print(2, "Israel 'Oliveira'")
+
+# Escape
+print("Israel \"Olivera\"")
+
+# r
+print(r"Israel \"Olivera\"")
