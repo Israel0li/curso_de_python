61e1c2ae23fc6b2146cc737ce6f6a6597349781b
Israel0li <israeloliveirarocha1@gmail.com>
2024-12-23 14:42:44 -0300

    .

--- aula12.py
+++ aula12.py
@@ -1,8 +1,15 @@
 nome = 'Luiz Otávio'
 altura = 1.80
 peso = 95
-imc = ...
+imc =  peso / altura ** 2
+# imc =  peso / (altura * altura)
+
+print(f'{nome} tem {altura} de altura,')
+print(f'pesa {peso} quilos e seu IMC é')
+print(imc)
 
 # Luiz Otávio tem 1.80 de altura,
 # pesa 95 quilos e seu IMC é
 # 29.320987654320987
+
+# Feito em 23/12/24
