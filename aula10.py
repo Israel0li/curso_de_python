d77400ff906fc52956d51fc5f2c1578d9024c332
Israel0li <israeloliveirarocha1@gmail.com>
2024-12-22 21:46:39 -0300

    Aula 10 finalizada.

---
+++ aula10.py
@@ -0,0 +1,11 @@
+# usando operadores aritmeticos em strings:
+
+concatenacao = 'A' + 'B' + 'C' # Concatenação
+print(concatenacao)
+
+a_vezes_dez = 'A' * 10 # Repetição
+israel_tres_vezes = 3 * 'Israel'
+print(a_vezes_dez)
+print(israel_tres_vezes)
+
+# Feito em 22/12/24
--- aula9.py
+++ aula9.py
@@ -7,7 +7,7 @@ print(f'Subtração {subtracao}')
 multiplicacao = 10 * 10
 print(f'Multiplicação {multiplicacao}')
 
-divisao = 10 / 2.2
+divisao = 10 / 2.2 # float
 print(f'Divisão {divisao}')
 
 divisao_inteira = 10 // 2.2
@@ -18,5 +18,12 @@ print(f'Exponenciação {exponenciacao}')
 
 modulo = 55 % 2 # Resto da divisão
 print(f'Módulo {modulo}')
+
+# Testes com o %(Resto da divisão):
+
+print(f'10 % 8 é igual a zero? {10 % 8 == 0}')
+print(f'16 % 8 é igual a zero? {16 % 8 == 0}')
+print(f'10 é impar ou par? {10 % 2 == 0}')
+print(f'17 é impar ou par? {17 % 2 == 0}')
 
 # Feito em 22/12/24
