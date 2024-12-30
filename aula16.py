39ea9cbeda3bd5e04c564b3003cca210194b4a86
Israel0li <israeloliveirarocha1@gmail.com>
2024-12-30 00:11:38 -0300

    .

---
+++ aula16.py
@@ -0,0 +1,14 @@
+# if / elif      / else
+# se / se não se / se não
+entrada = input('Você quer "entrar" ou "sair"? ')
+
+if entrada == 'entrar':
+    print('Você entrou no sistema.')
+elif entrada == 'sair':
+    print('Você saiu do sistema.')
+else:
+    print('Você não digitou nem entrou e nem saiu.')
+
+print(' FORA DOS BLOCOS')
+
+# Feito em 29/12/24
