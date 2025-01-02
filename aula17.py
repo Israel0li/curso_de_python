0436623c6a2d6fee18ad5622560fb0f4f8a12305
Israel0li <israeloliveirarocha1@gmail.com>
2025-01-01 22:32:18 -0300

    Primeiros códigos do ano.

--- aula17.py
+++ aula17.py
@@ -1,3 +1,24 @@
 # if / elif      / else
 # se / se não se / se não
 
+condicao1 = False
+condicao2 = True
+# A condicao2 acima que será executado, porque é o primeiro True da hierarquia do quarteto de condições.
+condicao3 = False
+condicao4 = True
+
+
+if condicao1:
+	print('Código para condição 1')
+elif condicao2:
+	print('Código para condição 2')
+elif condicao3:
+	print('Código para condição 3')
+elif condicao4:
+	print('Código para condição 4')
+else:
+	print('Nenhuma condição foi satisfeita.')
+
+print('Fora do if')
+
+# Feite em 01/01/25
